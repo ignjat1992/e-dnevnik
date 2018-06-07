b0f13946ebe9eda176b3448763b7772fdb5312dc
ignjat1992 <marko.ignjatovic992@gmail.com>
2018-06-07 19:28:20 +0200

    Introduce datebase and remove HomeController

--- app/controllers/UcenikController.scala
+++ app/controllers/UcenikController.scala
@@ -3,8 +3,10 @@ package controllers
 import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import storage.MongoApi
 
-class UcenikController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
+class UcenikController @Inject()(val mongoApi: MongoApi, cc: ControllerComponents) extends
+  AbstractController(cc) with I18nSupport {
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
---
+++ app/storage/MongoApi.scala
@@ -0,0 +1,35 @@
+package storage
+
+import javax.inject.{Inject, Singleton}
+import model.Ucenik
+import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.api.Cursor
+import reactivemongo.play.json.collection.JSONCollection
+import play.api.libs.json._
+
+import scala.concurrent.Future
+
+@Singleton
+class MongoApi @Inject()(val reactiveMongoApi: ReactiveMongoApi) {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val myDb = reactiveMongoApi.database
+  val ucenici: Future[JSONCollection] = myDb.map(_.collection("ucenici"))
+
+  def vratiSveUcenike() = {
+    ucenici.flatMap(_.find(Json.obj())
+      .cursor[Ucenik]()
+      .collect[Seq](-1, Cursor.FailOnError[Seq[Ucenik]]()))
+  }
+
+  def snimiUcenika(ucenik: Ucenik) = {
+    ucenici.flatMap(_.insert(ucenik))
+  }
+
+  def vratiJednogUcenika(id: String) = {
+    val select = Json.obj("_id" -> id)
+
+    ucenici.flatMap(_.find(select).one[Ucenik])
+  }
+}
